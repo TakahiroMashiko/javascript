8f97b47be0f687104e2597859fb10906a1c206bd
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-09-09 14:48:50 +0000

    [Add] Add chapter3/app1.js

--- chapter3/app.js
+++ chapter3/app.js
@@ -1,14 +1,3 @@
-// 関数
+let promptStr = prompt('何か好きな文字を入力してください。');
 
-// メイン部分
-let alertString;
-alertString = addString("WebCamp");
-
-// 作成した関数を呼び出す
-alert(alertString)
-
-// 作成した関数
-function addString(strA){
-  let addStr = "Hello " + strA;
-  return addStr;
-}
+alert(promptStr);
