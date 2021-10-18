af8ac0e9715916933491d822ab405ef956e5176f
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-10-18 14:36:13 +0000

    [Update] Add .addClass({'box1-ext'}); to chapter6/app.js

--- chapter6/app.js
+++ chapter6/app.js
@@ -1,6 +1,6 @@
 $(function(){
   $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000FF'});
+    $('.box1').addClass({'box1-ext'});
   });
   $('.box1').mouseout(function(){
     $('.box1).css({'background-color': '#FF0000'});
