34748399ecda8cbb90ecb8a0e4a0e650d0b480f3
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-12-16 13:28:20 +0000

    [Update] Add hidePrevious : false to chapter8/script.js

--- chapter8/script.js
+++ chapter8/script.js
@@ -18,4 +18,6 @@ $(".theTarget").skippr({
   autoPlayDuration : 5000,
   // キーボードの矢印キーによるスライド送りの設定（trueで有効）
   keyboardOnAlways : true,
+  // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
+  hidePrevious : false
 });
