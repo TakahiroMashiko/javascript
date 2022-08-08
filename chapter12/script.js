8d9b5c6dea6753e1c28942ef7e29d6d58d2634bf
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2022-08-08 12:06:27 +0900

    [Update] Add url: https://api.openweathermap.org/data/2.5/weather?q= + .val() +

--- chapter12/script.js
+++ chapter12/script.js
@@ -4,7 +4,7 @@ $(function(){
   $('bin').on('click', function() {
       // 入力された都市名でWebAPIに天気情報をリクエスト
     $.ajax({
-
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
     })
   });
 });
