5612fff15e921b633ac0243602bf10f4b7f74ecf
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2022-08-09 10:30:49 +0900

    [Update] Add dataType : 'jsonp', to chapter12/script.js

--- chapter12/script.js
+++ chapter12/script.js
@@ -5,6 +5,7 @@ $(function(){
       // 入力された都市名でWebAPIに天気情報をリクエスト
     $.ajax({
       url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
     })
   });
 });
