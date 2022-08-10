f2beb369c6423b9d5370bf961da0ac9dca832a6d
Takahiro Mashiko <takahiromashiko3122@gmail.com>
2022-08-10 10:20:53 +0900

    [Update] Add }).done(function (data){ to chapter12/script.js

--- chapter12/script.js
+++ chapter12/script.js
@@ -6,6 +6,7 @@ $(function(){
     $.ajax({
       url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
       dataType : 'jsonp',
+    }).done(function (data){
     })
   });
 });
